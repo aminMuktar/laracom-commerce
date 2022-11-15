f2074bf1c41adf7eb79552115448fbff42edae61
aminMuktar <aminmuktar41@gmail.com>
2022-11-15 10:12:07 +0300

    models created

---
+++ laracom-commerce/database/migrations/2022_11_15_065810_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',2000);
+            $table->string('slug',2000);
+            $table->string('image',2000)->nullable();
+            $table->string('image_mime')->nullable();
+            $table->string('image_size')->nullable();
+            $table->string('description',2000)->nullable();
+            $table->decimal('price',10,2);
+            $table->string('title',2000);
+            $table->foreignIdFor(User::class,'created_by')->nullable();
+            $table->foreignIdFor(User::class,'updated_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
